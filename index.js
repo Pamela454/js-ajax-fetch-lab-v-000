0b3c1a1b12ce13ea4b0161b4943f963fb48b9c98
Pamela Frieze <pfrieze@gmail.com>
2019-03-19 03:03:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function createIssue() {
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  const 
+  const issuePage = ''
   
   fetch('json.html_url')
   .then(response => response.json())
