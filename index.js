9fd395bf78928f40028f51944a40647139f3d651
Pamela Frieze <pfrieze@gmail.com>
2019-03-19 02:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ function createIssue() {
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
-  
+  fetch('json.html_url')
+  .then(response => response.json())
+  .then(json => console.log(json));
   
 }
