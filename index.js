a8370608c7cfd8bfaa0f6d6c67dd20bf634b99ce
Pamela Frieze <pfrieze@gmail.com>
2019-03-19 02:26:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function showResults(json) {
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
   const repo = '/js-ajax-fetch-lab';
-  const testBody = '';
+  const Body = 'testBody';
   fetch(`${repo}/issues`, {
     method: 'POST',
   headers: {
